88e46595a8236b33bfe59f46f89a2d0dff5af289
RonaldKemper <ronspoolco@gmail.com>
2022-06-19 19:13:49 -0400

    added user routes to backend, made users table in database

--- PluggedinFrontend/src/app/app-routing.module.ts
+++ PluggedinFrontend/src/app/app-routing.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Screens/login/login.component';
-
+import { CreateAccountComponent } from './Screens/create-account/create-account.component';
+import { MusicianRosterComponent } from './Screens/musician-roster/musician-roster.component';
+import { CreateBandComponent } from './Screens/create-band/create-band.component';
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
   },
-  // {
-  //   path: 'createAccount',
-  //   component: createAccountComponent
-  // },
-  // {
-  //   path: 'musicianRoster',
-  //   component: musicianRosterComponent
-  // },
+  {
+    path: 'create-account',
+    component: CreateAccountComponent,
+  },
+  {
+    path: 'musician-roster',
+    component: MusicianRosterComponent,
+  },
+  {
+    path: 'create-band',
+    component: CreateBandComponent,
+  },
   {
     path: '',
     redirectTo: '/',
