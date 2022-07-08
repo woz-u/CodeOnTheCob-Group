939ea8b1888be83063e6de5958ea4430ee7a618b
RonaldKemper <ronspoolco@gmail.com>
2022-07-07 23:29:07 -0400

    stage 1/2 front-back integration

--- PluggedinFrontend/src/app/app-routing.module.ts
+++ PluggedinFrontend/src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Screens/login/login.component';
+import { BandProfileComponent } from './Screens/band-profile/band-profile.component';
 import { CreateAccountComponent } from './Screens/create-account/create-account.component';
-import { MusicianRosterComponent } from './Screens/musician-roster/musician-roster.component';
 import { CreateBandComponent } from './Screens/create-band/create-band.component';
+
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
   },
   {
-    path: 'create-account',
-    component: CreateAccountComponent,
+    path: 'band-profile',
+    component: BandProfileComponent,
   },
   {
-    path: 'musician-roster',
-    component: MusicianRosterComponent,
+    path: 'create-account',
+    component: CreateAccountComponent,
   },
   {
     path: 'create-band',
