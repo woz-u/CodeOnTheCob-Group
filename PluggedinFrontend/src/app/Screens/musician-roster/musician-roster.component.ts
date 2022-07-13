81f440a61a2dcede732c12a6b01ec67bbecdbcde
RonaldKemper <ronspoolco@gmail.com>
2022-07-13 16:39:17 -0400

    added temporary files for testing purposes

--- PluggedinFrontend/src/app/Screens/musician-roster/musician-roster.component.ts
+++ PluggedinFrontend/src/app/Screens/musician-roster/musician-roster.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-musician-roster',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./musician-roster.component.css'],
 })
 export class MusicianRosterComponent implements OnInit {
-  constructor() {}
+  musicians: any;
 
-  ngOnInit(): void {}
+  getMusicians(){
+    this.musicianService.getAll().subscribe((musician) => (this.musicians = musician));
+  }
+
+  constructor(private musicianService: LoginService) {}
+
+  ngOnInit(): void {
+    this.getMusicians()
+  }
 }
